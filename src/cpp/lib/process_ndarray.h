72bd522a240dad1011926efc0a34193f0bd067ac
luckyjim <jcolley@lpnhe.in2p3.fr>
2026-06-28 19:07:59 +0200

    add interface for class used ndarray

---
+++ src/cpp/lib/process_ndarray.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class ProcessNdarray {
+public:
+    ProcessNdarray() = default;
+
+    // add scalar for all elements of array 
+    int add_in_place(??  array, double scalar);
+    // print shape and dtype
+    int inspect(?? array) const;
+};
