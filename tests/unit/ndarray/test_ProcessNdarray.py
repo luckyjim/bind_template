4496db737f1f27536835b1e8561448261ec64d83
luckyjim <jcolley@lpnhe.in2p3.fr>
2026-06-28 22:37:34 +0200

    add test

---
+++ tests/unit/ndarray/test_ProcessNdarray.py
@@ -0,0 +1,10 @@
+from bind_template.ndarray import ProcessNdarray
+import numpy as np
+
+
+def test_ProcessNdarray_add_in_place():
+    arr = np.arange(10, dtype=np.float64)
+    obj = ProcessNdarray()
+    obj.add_in_place(arr, 5)
+    assert np.all(arr == np.arange(5, 15, dtype=np.float64))
+    
